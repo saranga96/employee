a722330e55f12fd87785f46be290ab3aa5816221
saranga96 <sarangamadhushan1996@gmail.com>
2022-07-15 17:37:28 +0530

    employee

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+      <HeaderComponent/>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component ={ListEmployeeComponent}/>
+          <Route path="/employees" component ={ListEmployeeComponent}/>
+          <Route path="/add-employee" component={AddEmployeeComponent}/>
+          <Route path="/edit-employee/:id" component={AddEmployeeComponent}/>
+          </Switch>
+      </div>
+      <FooterComponent/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
